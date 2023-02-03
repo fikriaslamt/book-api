588c84d60146ec415b3f2541053557f716d24083
fikri <fikri.aslam102519@students.unila.ac.id>
2023-02-03 19:25:03 +0700

    fix cors

--- api/api.go
+++ api/api.go
@@ -20,7 +20,7 @@ func Routes() {
 
 	config := cors.DefaultConfig()
 	config.AllowCredentials = true
-	config.AllowOrigins = []string{"http://localhost:3000/", "http://google.com"}
+	config.AllowOrigins = []string{"http://114.79.6.174:3000/", "http://google.com"}
 
 	r.Use(cors.New(config))
 	//user routes
